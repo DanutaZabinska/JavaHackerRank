38362bb9e20a38d61140c68df61bc9bd4f44ada6
DanutaZabinska <danuta.irena.zabinska@gmail.com>
2021-02-03 18:56:49 +0100

    final version_challenge_passed

--- StaticInitializerBlock/src/com/hackerrank/javastaticinitializerblock/Main.java
+++ StaticInitializerBlock/src/com/hackerrank/javastaticinitializerblock/Main.java
@@ -3,27 +3,26 @@ package com.hackerrank.javastaticinitializerblock;
 import java.util.Scanner;
 
 public class Main {
-    static double B;
-    static double H;
+    static int B;
+    static int H;
+    static boolean flag;
 
     static {
         Scanner sc = new Scanner(System.in);
         B = sc.nextInt();
         H = sc.nextInt();
+      if( B <= 0 | H <= 0)
+          System.out.println("java.lang.Exception: Breadth and height must be positive");
+      else if(B > 100 | H > 100)
+          flag = false;
+      else
+          flag = true;
     }
 
-    public static void main(String[] args) {
-        if(B <= 0 | H <= 0)
-        {
-            System.out.println("java.lang.Exception: Breadth and height must be positive");
-        }
-        else if (B > 100 | H > 100)
-        {
-            System.out.println("Numbers are too big");
-        }
-        else
-        {
-            System.out.println(B * H);
+    public static void main(String[] args){
+        if(flag){
+            int area = B*H;
+            System.out.print(area);
         }
 
     }
