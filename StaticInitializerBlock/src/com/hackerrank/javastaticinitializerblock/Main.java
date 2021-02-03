2cdcb5d433cb68cb1b5c156486d6dbd83eb5f3ed
DanutaZabinska <danuta.irena.zabinska@gmail.com>
2021-02-03 18:44:05 +0100

    usign Initioalizer Block

--- StaticInitializerBlock/src/com/hackerrank/javastaticinitializerblock/Main.java
+++ StaticInitializerBlock/src/com/hackerrank/javastaticinitializerblock/Main.java
@@ -3,11 +3,16 @@ package com.hackerrank.javastaticinitializerblock;
 import java.util.Scanner;
 
 public class Main {
+    static double B;
+    static double H;
+
+    static {
+        Scanner sc = new Scanner(System.in);
+        B = sc.nextInt();
+        H = sc.nextInt();
+    }
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        double B = sc.nextInt();
-        double H = sc.nextInt();
         if(B <= 0 | H <= 0)
         {
             System.out.println("java.lang.Exception: Breadth and height must be positive");
